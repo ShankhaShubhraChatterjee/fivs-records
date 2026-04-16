93a7327899ac9a1167e93bb820008d1d9df71ced
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmaill.com>
2026-04-16 16:42:21 +0530

    feat: add login page

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -1,7 +1,58 @@
-export default function AdminLogin () {
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export default function Login() {
     return (
-        <div>
-            Admin Login Page
+        <div className="w-full h-full flex-1 p-2 mt-4 flex justify-center items-center">
+            <Card className="w-full max-w-md">
+                <CardHeader>
+                    <CardTitle className="text-2xl font-bold">Login To Your Account</CardTitle>
+                    <CardDescription>
+                        Enter your credentials below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="username">Username</Label>
+                                <Input
+                                    id="username"
+                                    type="text"
+                                    placeholder="Username ..."
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                                <Input id="password" type="password" required placeholder="Password ..." />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                    <Button type="submit" className="w-full">
+                        Login
+                    </Button>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
