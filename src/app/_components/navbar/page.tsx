c85e626440bb706f71456e48098a9af3bccbd73e
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmaill.com>
2026-04-16 13:30:37 +0530

    chore: add shadcn

--- src/app/_components/navbar/page.tsx
+++ src/app/_components/navbar/page.tsx
@@ -11,20 +11,20 @@ export default function Navbar() {
     ]
     return (
         <div className="flex justify-between items-center px-2 py-2 backdrop-blur-md mt-2 bg-zinc-900 rounded-4xl mx-2">
-            <section className="flex justify-center items-center">
+            <Link href="/" className="flex justify-center items-center">
                 <span className="mr-2 bg-indigo-700 rounded-full p-1">
                     <Image src={UmbrellaLogo} alt="Umbrella Logo" />
                 </span>
 
-                <h1 className="text-xl font-bold">Fivs Records</h1>
-            </section>
+                <h1 className="text-xl font-bold text-white">Fivs Records</h1>
+            </Link>
 
 
             <section className="flex justify-center items-center">
 
                 <nav className="mr-2">
                     {navigationLinks.map((link) => {
-                        return <Link key={link.id} href={link.url} className="font-bold px-5 py-2.5 mr-1.5 rounded-4xl bg-gray-700/50">{link.name}</Link>
+                        return <Link key={link.id} href={link.url} className="text-white font-bold px-5 py-2.5 mr-1.5 rounded-4xl bg-gray-700/50">{link.name}</Link>
                     })}
                 </nav>
                 <button className="bg-indigo-700 p-2 rounded-full cursor-pointer">
