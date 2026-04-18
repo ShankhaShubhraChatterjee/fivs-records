170dd7f5379d2389dedd705de9a5d09e76913ba1
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmaill.com>
2026-04-18 23:56:14 +0530

    feat: add terms and conditions page

---
+++ src/app/terms_and_conditions/page.tsx
@@ -0,0 +1,10 @@
+export default function TermsAndConditions() {
+    return (
+        <div className="flex-1">
+            <div className="mt-24 ml-4">
+                <p>Terms & Conditions</p>
+            </div>
+        </div>
+
+    )
+}
