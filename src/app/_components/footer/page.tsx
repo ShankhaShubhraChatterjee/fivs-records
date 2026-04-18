0b877906b21f6b545a17788621d1c804d6109f5a
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmaill.com>
2026-04-18 23:56:00 +0530

    feat: change background colors and add more styles

--- src/app/_components/footer/page.tsx
+++ src/app/_components/footer/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link"
 export default function Footer () {
     return (
-        <footer className="text-center p-3 bg-zinc-900/75 text-white w-full">
-            &copy; S.S.Chatterjee, 2026 All Rights Reserved. <Link href="/" className="text-blue-500">Terms & Conditions</Link> apply.
+        <footer className="text-center p-3 shadow-lg backdrop-blur-lg mt-2 bg-white/1 dark:text-white w-full">
+            &copy; S.S.Chatterjee, 2026 All Rights Reserved. <Link href="/terms_and_conditions" className="text-blue-500">Terms & Conditions</Link> apply.
         </footer>
     )
 }
