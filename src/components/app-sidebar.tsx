889140bf3bb7df4f63b233df585a800b965e3b47
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmaill.com>
2026-04-23 21:57:40 +0530

    feat: change sidebar width for desktop, add links to sidebar items

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+import FooterCard from "@/app/_components/footer_card/page"
+import Header from "@/app/_components/header/page"
 import {
   Sidebar,
   SidebarContent,
@@ -16,25 +18,31 @@ import Link from "next/link"
 export default function AppSidebar() {
   return (
     <Sidebar className="z-10">
-      <SidebarHeader>
+      <SidebarHeader className="mt-3">
+        <Header />
+      </SidebarHeader>
+      <SidebarContent className="mt-2">
         <SidebarGroup>
-          <SidebarGroupLabel>Account Navigations</SidebarGroupLabel>
+          <SidebarGroupLabel className="mb-2">Account Navigations</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               <SidebarMenuItem>
-                <SidebarMenuButton>
-                 HOME
+                <SidebarMenuButton className="font-bold">
+                  <Link href="/user/admin/clients">Clients & Investors</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="font-bold">
+                  Overview
                 </SidebarMenuButton>
               </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
-      </SidebarHeader>
-      <SidebarContent>
-        {/* Main content */}
       </SidebarContent>
       <SidebarFooter>
         {/* Footer content */}
+        <FooterCard />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
