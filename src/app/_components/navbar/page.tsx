2e4a9871342a0cfd5f0cf27150171022f01e5723
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmaill.com>
2026-04-23 21:54:30 +0530

    feat: replace title with header component, add conditional rendering of navbar links

--- src/app/_components/navbar/page.tsx
+++ src/app/_components/navbar/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import UmbrellaLogo from '@/../public/umbrella.svg';
 import TranslationLogo from '@/../public/translation.svg';
 import MenuLogo from '@/../public/menu.svg';
 import ThemeToggleButton from "@/components/theme-provider";
@@ -16,28 +15,27 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import Header from "../header/page";
 
 export default function Navbar() {
 
 
     const navigationLinks = [
-        { id: 1, name: "Login", url: "/user/login" },
-        { id: 2, name: "Contact Us", url: "/contact" }
+        { id: 1, name: "Login", url: "/user/login", hide: false },
+        { id: 2, name: "Log Out", url: "/user/logout", hide: false },
+        { id: 3, name: "Contact Us", url: "/contact", hide: false }
     ]
     return (
         <div className="fixed top-1 w-[calc(100%-10px)] flex justify-between items-center px-2 py-2 shadow-lg backdrop-blur-lg mt-2 bg-white/1 rounded-4xl mx-2">
-            <Link href="/" className="flex justify-center items-center">
-                <span className="mr-2 bg-indigo-700 rounded-full p-1">
-                    <Image loading="eager" src={UmbrellaLogo} alt="Umbrella Logo" />
-                </span>
-
-                <h1 className="text-xl font-bold dark:text-white light:text-black">Fivs Records</h1>
-            </Link>
+            <Header />
             <section className="flex justify-center items-center">
 
                 <nav className="mr-2 hidden sm:block">
                     {navigationLinks.map((link) => {
-                        return <Link key={link.id} href={link.url} className="dark:text-white font-bold px-5 py-2.5 mr-1.5 rounded-4xl bg-gray-200 hover:text-gray-100 hover:bg-indigo-700 transition dark:bg-gray-200/10 dark:hover:bg-indigo-700">{link.name}</Link>
+                        if (!link.hide) {
+                            return <Link key={link.id} href={link.url} className="dark:text-white font-bold px-5 py-2.5 mr-1.5 rounded-4xl bg-gray-200 hover:text-gray-100 hover:bg-indigo-700 transition dark:bg-gray-200/10 dark:hover:bg-indigo-700">{link.name}</Link>
+                        }
+
                     })}
                 </nav>
                 <button className="bg-indigo-700 p-2 rounded-full cursor-pointer mr-2">
@@ -45,21 +43,21 @@ export default function Navbar() {
                 </button>
                 <ThemeToggleButton />
                 <DropdownMenu>
-                        <DropdownMenuTrigger asChild className="sm:hidden block">
-                            <Button className="rounded-full bg-indigo-700 ml-2 size-10 hover:bg-indigo-800" variant="default">
-                                <Image loading="eager" width={24} height={24} src={MenuLogo} alt="Menu Icon" />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="relative right-2 top-4">
-                            <DropdownMenuItem asChild>
-                                <Link href="/user/login">Login</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem asChild>
-                                <Link href="/contact">Contact</Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem disabled>Log Out</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+                    <DropdownMenuTrigger asChild className="sm:hidden block">
+                        <Button className="rounded-full bg-indigo-700 ml-2 size-10 hover:bg-indigo-800" variant="default">
+                            <Image loading="eager" width={24} height={24} src={MenuLogo} alt="Menu Icon" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="relative right-2 top-4">
+                        <DropdownMenuItem asChild>
+                            <Link href="/user/login">Login</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Link href="/contact">Contact</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem disabled>Log Out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </section>
         </div>
     )
